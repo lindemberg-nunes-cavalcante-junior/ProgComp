e1340ce5009cb5db2f1f0f0d3a9795b23e418c37
lindemberg <lindembergjnr@gmail.com>
2024-08-15 20:57:08 -0300

    continuo depois

---
+++ Atividade_3(Lista)/Questao_02.py
@@ -0,0 +1,11 @@
+'''
+2. (Valor: 5 pontos) Faça um programa que leia dois valores: x e n (x e n deverão ser solicitados ao 
+usuário), onde x é a quantidade de elementos que a lista deverá armazenar positivo e n serão os valores 
+inteiros a serem inseridos na lista, o programa deve terminar a leitura dos números quando for informado 
+o valor 0 (o valor 0 não deverá fazer parte da lista). A lista só deverá armazenar os x menores números 
+informados, seguindo a lógica abaixo:
+'''
+
+x = int(input('Informe a quantidade de elementos da lista:'))
+
+lista = list()
