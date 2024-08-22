b91d1921ae8cc3d0610f933efa43aa5eaa4fdb82
Lindemberg Júnior <lindembergjnr@gmail.com>
2024-08-21 22:51:58 -0300

    2 feito

--- Atividade_3(Lista)/Questao_02.py
+++ Atividade_3(Lista)/Questao_02.py
@@ -5,16 +5,22 @@ inteiros a serem inseridos na lista, o programa deve terminar a leitura dos núm
 o valor 0 (o valor 0 não deverá fazer parte da lista). A lista só deverá armazenar os x menores números 
 informados, seguindo a lógica abaixo:
 '''
+import sys
 
 x = int(input('Informe a quantidade de elementos da lista:'))
+
+if x <= 0:
+    sys.exit('Informe um valor maior que 0.')
+
 lista = list()
 n = 1
 
 while n != 0:
     n = int(input('Informe um valor:'))
-
-    if len(lista) < n:
+    
+    if len(lista) < x:
         lista.append(n)
-
+    elif lista[x-1] > n:
+        lista[x-1] = n
     lista.sort()
     print(lista)
