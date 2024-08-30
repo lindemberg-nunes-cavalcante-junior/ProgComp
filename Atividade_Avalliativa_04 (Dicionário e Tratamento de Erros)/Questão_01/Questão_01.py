283152dfbeb2267fc433e0170fed1a15fec72a28
Lindemberg <lindembergjnr@gmail.com>
2024-08-30 14:15:38 -0300

    Começando atividade 4

---
+++ Atividade_Avalliativa_04 (Dicionário e Tratamento de Erros)/Questão_01/Questão_01.py
@@ -0,0 +1,14 @@
+import requests
+
+strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata' 
+strURL += '/Moedas?$top=100&$format=json' 
+dictMoedas = requests.get(strURL).json() 
+strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
+strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
+strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
+strURL += '@moeda=%27USD%27&@dataInicial=%2701-01-2023%27&' 
+strURL += '@dataFinalCotacao=%2712-31-2023%27&$top=100&$format=json' 
+dictCotacoes = requests.get(strURL).json() 
+
+
+print(dictCotacoes)
