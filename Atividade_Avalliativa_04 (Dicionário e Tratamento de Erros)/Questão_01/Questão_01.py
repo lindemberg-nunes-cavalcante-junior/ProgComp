8dbd04427c090255a4b190de52b1fe65e5800eaa
lindemberg <lindembergjnr@gmail.com>
2024-09-02 20:56:37 -0300

    Continuando Q1

--- Atividade_Avalliativa_04 (Dicionário e Tratamento de Erros)/Questão_01/Questão_01.py
+++ Atividade_Avalliativa_04 (Dicionário e Tratamento de Erros)/Questão_01/Questão_01.py
@@ -1,21 +1,41 @@
-import requests
+import requests,sys, statistics
+from datetime import datetime
 
-strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata' 
-strURL += '/Moedas?$top=100&$format=json' 
-dictMoedas = requests.get(strURL).json() 
-strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
-strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
-strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
-strURL += '@moeda=%27USD%27&@dataInicial=%2701-01-2023%27&' 
-strURL += '@dataFinalCotacao=%2712-31-2023%27&$top=100&$format=json' 
-dictCotacoes = requests.get(strURL).json() 
+try:
+    strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata' 
+    strURL += '/Moedas?$top=100&$format=json' 
+    dictMoedas = requests.get(strURL).json()
+except:
+    print(f'\ERROR: {sys.exc_info()}') 
 
 
 
-cotacoes = [i for i in dictCotacoes['value']]
 
-moedas = [i for i in dictMoedas['value']]
+for i in dictMoedas['value']:
+    x = i['simbolo']
+    print(f'{x}')
+moeda = input(f'Digite a moeda escolhida: ')
+moedas = list(filter(lambda x: x['simbolo'] == moeda, dictMoedas['value']))
+if len(moedas) <= 0:
+    sys.exit('Moeda não existe')
 
-print(cotacoes)
+ano = int(input('Digite o ano desejado: '))
+if ano > int(datetime.now().year):
+    sys.exit('Coloque um ano válido e anterior, ou igual, ao atual.')
+
+try:
+    strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
+    strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
+    strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
+    strURL += f'@moeda=%27{moeda}%27&@dataInicial=%2701-01-{ano}%27&' 
+    strURL += f'@dataFinalCotacao=%2712-31-{ano}%27&$top=100&$format=json' 
+    dictCotacoes = requests.get(strURL).json()
+except:
+    print(f'\ERROR: {sys.exc_info()}')
+
+media_compra = list(map(lambda x: x['cotacaoCompra'], dictCotacoes['value']))
+media_venda = list(map(lambda x: x['cotacaoVenda'], dictCotacoes['value']))  
+
+print(media_compra)
 
 
