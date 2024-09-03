2aaf789c75be2c3013cadedceb2888acf1c15c3b
Lindemberg Júnior <lindembergjnr@gmail.com>
2024-09-03 11:09:14 -0300

    quase finalizando a Q1

--- Atividade_Avalliativa_04 (Dicionário e Tratamento de Erros)/Questão_01/Questão_01.py
+++ Atividade_Avalliativa_04 (Dicionário e Tratamento de Erros)/Questão_01/Questão_01.py
@@ -6,7 +6,7 @@ try:
     strURL += '/Moedas?$top=100&$format=json' 
     dictMoedas = requests.get(strURL).json()
 except:
-    print(f'\ERROR: {sys.exc_info()}') 
+    print(f'/ERROR: {sys.exc_info()}') 
 
 
 
@@ -23,19 +23,40 @@ ano = int(input('Digite o ano desejado: '))
 if ano > int(datetime.now().year):
     sys.exit('Coloque um ano válido e anterior, ou igual, ao atual.')
 
-try:
-    strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
-    strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
-    strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
-    strURL += f'@moeda=%27{moeda}%27&@dataInicial=%2701-01-{ano}%27&' 
-    strURL += f'@dataFinalCotacao=%2712-31-{ano}%27&$top=100&$format=json' 
-    dictCotacoes = requests.get(strURL).json()
-except:
-    print(f'\ERROR: {sys.exc_info()}')
+meses = ['janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
 
-media_compra = list(map(lambda x: x['cotacaoCompra'], dictCotacoes['value']))
-media_venda = list(map(lambda x: x['cotacaoVenda'], dictCotacoes['value']))  
+media_compra = list()
+media_venda = list()  
 
-print(media_compra)
+for i in range(1,13):
+
+    if i < 10:
+        try:
+            strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
+            strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
+            strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
+            strURL += f'@moeda=%27{moeda}%27&@dataInicial=%2701-0{i}-{ano}%27&' 
+            strURL += f'@dataFinalCotacao=%2712-31-{ano}%27&$top=100&$format=json' 
+            dictCotacoes = requests.get(strURL).json()
+        except:
+            print(f'/ERROR: {sys.exc_info()}')
+    else:
+        try:
+            strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
+            strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
+            strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
+            strURL += f'@moeda=%27{moeda}%27&@dataInicial=%2701-{i}-{ano}%27&' 
+            strURL += f'@dataFinalCotacao=%2712-31-{ano}%27&$top=100&$format=json' 
+            dictCotacoes = requests.get(strURL).json()
+        except:
+            print(f'/ERROR: {sys.exc_info()}')  
+    media_compra.append(list(map(lambda x: [x['cotacaoCompra'], datetime.strptime(x['dataHoraCotacao'],"%Y-%m-%d %H:%M:%S.%f")],dictCotacoes['value'])))
+    media_venda.append(list(map(lambda x: [x['cotacaoVenda'], datetime.strptime(x['dataHoraCotacao'],"%Y-%m-%d %H:%M:%S.%f")], dictCotacoes['value'])))
+
+
+Cotas = {value: {'MediaCompra':[x[1].month for x in compra], 'MediaVenda':[x[1].month for x in venda]} for value,i,compra,venda in zip(meses,range(1,13),media_compra,media_venda)}
+
+for i in Cotas.keys():
+    print(f'{i}:{Cotas[i]['MediaCompra']}')
 
 
