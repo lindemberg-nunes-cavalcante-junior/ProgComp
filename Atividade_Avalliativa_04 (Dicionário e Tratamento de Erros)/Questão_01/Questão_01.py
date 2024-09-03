e82db739426be563ee4825d1d34cb83b39671e71
lindemberg <lindembergjnr@gmail.com>
2024-09-03 20:58:30 -0300

    Finalizando a Q1

--- Atividade_Avalliativa_04 (Dicionário e Tratamento de Erros)/Questão_01/Questão_01.py
+++ Atividade_Avalliativa_04 (Dicionário e Tratamento de Erros)/Questão_01/Questão_01.py
@@ -1,4 +1,4 @@
-import requests,sys, statistics
+import requests,sys,statistics,json,os
 from datetime import datetime
 
 try:
@@ -9,54 +9,54 @@ except:
     print(f'/ERROR: {sys.exc_info()}') 
 
 
-
-
 for i in dictMoedas['value']:
     x = i['simbolo']
     print(f'{x}')
-moeda = input(f'Digite a moeda escolhida: ')
+# moeda = input(f'Digite a moeda escolhida: ')
+moeda = 'USD'
 moedas = list(filter(lambda x: x['simbolo'] == moeda, dictMoedas['value']))
 if len(moedas) <= 0:
     sys.exit('Moeda não existe')
 
-ano = int(input('Digite o ano desejado: '))
+# ano = int(input('Digite o ano desejado: '))
+ano = 2024
 if ano > int(datetime.now().year):
     sys.exit('Coloque um ano válido e anterior, ou igual, ao atual.')
 
-meses = ['janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
+try:
+    strURL = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/'
+    strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial='
+    strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?'
+    strURL += f'@moeda=%27{moeda}%27&@dataInicial=%2701-01-{ano}%27&'
+    strURL += f'@dataFinalCotacao=%2712-31-{ano}%27&$format=json'
+    dictCotacoes = requests.get(strURL).json()
+except:
+    print(sys.exc_info())
 
-media_compra = list()
-media_venda = list()  
+meses = ['janeiro','Fevereiro','Marco','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro']
 
-for i in range(1,13):
+medias = list(map(lambda x: [x['cotacaoCompra'], x['cotacaoVenda'],datetime.strptime(x['dataHoraCotacao'],"%Y-%m-%d %H:%M:%S.%f")],dictCotacoes['value']))
 
-    if i < 10:
-        try:
-            strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
-            strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
-            strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
-            strURL += f'@moeda=%27{moeda}%27&@dataInicial=%2701-0{i}-{ano}%27&' 
-            strURL += f'@dataFinalCotacao=%2712-31-{ano}%27&$top=100&$format=json' 
-            dictCotacoes = requests.get(strURL).json()
-        except:
-            print(f'/ERROR: {sys.exc_info()}')
-    else:
-        try:
-            strURL  = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/' 
-            strURL += 'CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=' 
-            strURL += '@dataInicial,dataFinalCotacao=@dataFinalCotacao)?' 
-            strURL += f'@moeda=%27{moeda}%27&@dataInicial=%2701-{i}-{ano}%27&' 
-            strURL += f'@dataFinalCotacao=%2712-31-{ano}%27&$top=100&$format=json' 
-            dictCotacoes = requests.get(strURL).json()
-        except:
-            print(f'/ERROR: {sys.exc_info()}')  
-    media_compra.append(list(map(lambda x: [x['cotacaoCompra'], datetime.strptime(x['dataHoraCotacao'],"%Y-%m-%d %H:%M:%S.%f")],dictCotacoes['value'])))
-    media_venda.append(list(map(lambda x: [x['cotacaoVenda'], datetime.strptime(x['dataHoraCotacao'],"%Y-%m-%d %H:%M:%S.%f")], dictCotacoes['value'])))
+# Guardando as medias em um dicionário, onde a chave é o mes correspondente
+Medias = {value: {'MediaCompra':round((statistics.mean([x[0] if x[2].month == mes else 0 for x in medias])),5),'MediaVenda':round((statistics.mean([x[1] if x[2].month == mes else 0 for x in medias])),5)} for value,mes in zip(meses,range(1,13))}
 
+# Salvando os arquivos
+direntrada = os.path.abspath(__file__)
+direntrada = os.path.dirname(direntrada)
 
-Cotas = {value: {'MediaCompra':[x[1].month for x in compra], 'MediaVenda':[x[1].month for x in venda]} for value,i,compra,venda in zip(meses,range(1,13),media_compra,media_venda)}
+# JSON
+arqentrada = open(direntrada + f'\\medias_cotacoes_{moeda}_{ano}.json','w',encoding='utf-8')
+json.dump(Medias,arqentrada,indent=6)
+arqentrada.close()
 
-for i in Cotas.keys():
-    print(f'{i}:{Cotas[i]['MediaCompra']}')
+#CVC
+arqentrada = open(direntrada + f'\\medias_cotacoes_{moeda}_{ano}.cvs','w',encoding='utf-8')
+arqentrada.write('moeda;mes;mediaCompra;mediaVenda;\n')
+for i in Medias.keys():
+    x = Medias[i]['MediaCompra']
+    y = Medias[i]['MediaVenda']
+    arqentrada.write(f'{moeda};{i};{x};{y}\n')
+arqentrada.close()
+
 
 
